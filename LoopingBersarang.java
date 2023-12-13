54048ffb01622898762e28bf440f286b5edfac24
muhammadbisma21 <153070381+muhammadbisma21@users.noreply.github.com>
2023-12-13 12:03:28 +0700

    ini contoh change file 1

--- LoopingBersarang.java
+++ LoopingBersarang.java
@@ -5,7 +5,7 @@ public class LoopingBersarang {
     public static void main(String[] args) {
         for(int i=0; i<5; i++){
             for(int j=0; j<5; j++){
-                System.out.print("* ");
+                System.out.print("# ");
             }
             System.out.print("\n");
         }
@@ -14,7 +14,7 @@ public class LoopingBersarang {
 
         for(int i=0; i<5; i++){
             for(int j=0; j<5; j++){
-                System.out.print("* ");
+                System.out.print("# ");
                 if (i==j){
                     break;
                 }
